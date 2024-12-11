667ca9625b0cfc5cea35e23636580fdbf1b33698
Harish-dev22 <cheyyarharish22@gmail.com>
2024-12-11 23:50:26 +0530

    Added HomeComponent route to application routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/register', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomeComponent},
   {
     path: '**',           // Wildcard route for invalid paths
     redirectTo: 'register'   // Redirects to login component
