9d97bf6b760d2651a67062b69e97b94b753690c4
Harish-dev22 <cheyyarharish22@gmail.com>
2024-12-12 09:20:43 +0530

    Multiple layouts implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LayoutComponent } from './layout/layout.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [LayoutComponent],
+  imports: [LayoutComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,25 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LayoutComponent } from './layout/layout.component';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/register', pathMatch: 'full' },
-  { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomeComponent},
   {
-    path: '**',           // Wildcard route for invalid paths
-    redirectTo: 'register'   
-  }
+    path: '',
+    component: AuthLayoutComponent, // Use AuthLayout for these routes
+    children: [
+        { path: '', component: RegisterComponent },
+        { path: 'register', component: RegisterComponent },
+    ]
+},
+  {
+    path: '',
+    component: LayoutComponent, // Use Layout for the Home component
+    children: [
+        { path: 'home', component: HomeComponent }, // Home route
+        // Other routes that should use LayoutComponent can go here
+    ]
+},
+{ path: '**', redirectTo: 'register' } // Redirect to home for unknown routes
 ];
